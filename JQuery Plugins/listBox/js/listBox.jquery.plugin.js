8f8522b975205e1ba0b2f9313b26f3403e85552c
RenatiusvanSander <remad@web.de>
2024-05-12 20:44:36 +0200

    master: listBox

--- JQuery Plugins/listBox/js/listBox.jquery.plugin.js
+++ JQuery Plugins/listBox/js/listBox.jquery.plugin.js
@@ -5,12 +5,24 @@
         }, options );
 
         var _that = this;
+        _that.settings;
+
+        var element = $("#listBox");
+        element.addClass('listBox');
+
+        var ul = $('<ul id="listBox" class="review_bottom"></ul>');
+        var input = $('<input type="text" />').appendTo("body").on('keydown', function(event) {
+            console.log(event);
+        });
+        for(i = 0; i < 10; i++) {
+            ul.append($('<il id=' + i + '>' + i + '</li>'));
+        }
+        element.append(ul);
 
         return this;
     };
 }( jQuery ));
 
 $( document ).ready(function() {
-     var bar = $( "<div></div>" )
-     .appendTo( "body" ).listBox();
+     var bar = $.fn.listBox();
 });
