03e97022ea50a585bf1c62c7b01caa3c4a399878
Remy Meier <remad@web.de>
2024-06-03 23:35:18 +0200

    master: working on arrow down key

--- JQuery Plugins/listBox/js/listBox.jquery.plugin.js
+++ JQuery Plugins/listBox/js/listBox.jquery.plugin.js
@@ -19,26 +19,44 @@
 
             if(event.originalEvent.keyCode === 13) {
                 if(element.find('#listBoxUl').is(':visible')) {
-                    element.find('listBoxUl').hide();
+                    let inputValue = $(listBoxChildren[positionIndex]).text();
+                    input.val(inputValue);
+                    $(listBoxChildren[positionIndex]).removeClass('focus-visible').blur();
+
+                    var list = element.find('#listBoxUl');
+                    $(list).css('visibility','hidden');
+                    $(list).css('width','0px');
+                    $(list).css('height','0px');
+                    input.focus({focusVisible: true})
                 }
             }
     
             if(event.originalEvent.keyCode === 40 && event.originalEvent.altKey === true) {
-                element.find('listBoxUl').show();
+                element.find('#listBoxUl').show();
             }
 
             if(event.originalEvent.keyCode === 27) {
-                if(element.find('listBoxUl').is(':visible')) {
-                    element.find('listBoxUl').hide();
+                if($(element).find('#listBoxUl')[0].hasAttribute('style') === true) {
+                    input.val('');
+
+                    $(listBoxChildren[positionIndex]).removeClass('focus-visible').blur();
+
+                    var list = element.find('#listBoxUl');
+                    $(list).css('visibility','hidden');
+                    $(list).css('width','0px');
+                    $(list).css('height','0px');
+                    input.focus({focusVisible: true})
+                    positionIndex = 0;
                 }
 
-                if(element.find('listBoxUl').not(':visible')) {
-                    input.val('');
+                if($(element).find('listBoxUl')[0].hasAttribute('style') === false) {
+                    element.find('listBoxUl').hide();
                 }
             }
 
             if(event.originalEvent.keyCode === 40 && event.originalEvent.altKey === false) {
-                if(element[0].children[1].hasAttribute('style') && input.val().length === 0) {
+                if(element[0].children[1].hasAttribute('style')) {
+                    let vis = $(element).children()[1].attr('style');
                     $(listBoxChildren[positionIndex]).removeClass('focus-visible');
                     $(listBoxChildren[positionIndex]).blur();
 
@@ -50,6 +68,8 @@
                     $(list).css('visibility','visible');
                     $(list).css('width','40px');
                     $(list).css('height','80px');
+
+                    //positionIndex = positionIndex < 9 ? positionIndex + 1 : 0;
 
                     listBoxChildren[positionIndex].focus({focusVisible: true});
                     $(listBoxChildren[positionIndex]).addClass('focus-visible');
