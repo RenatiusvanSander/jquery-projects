39f8508aa9983f36f06b49a7cbacd8f244c991ef
RenatiusvanSander <remad@web.de>
2024-09-18 07:38:18 +0200

    master: slideshow can slide forward and back

--- JQuery Plugins/slideshowproject/scripts/slider.jquery.js
+++ JQuery Plugins/slideshowproject/scripts/slider.jquery.js
@@ -11,6 +11,42 @@ function($) {
         forward: $allButtons.filter(".forward"),
         back: $allButtons.filter(".back")
       };
+
+      var endMargin = ($sliderItems.length - 1) * $sliderItems.first().children("img").width();
+
+      $allButtons.on("click", function(event) {
+        var isBackButton = $(this).hasClass("back");
+        if(!isBackButton && isAtEnd()) {
+          animateSliderToMargin(0, 1000);
+        } else {
+          animateSlider((isBackButton ? "+" : "-"), 1000);
+        }
+        event.preventDefault();
+      });
+
+      var getLeftMargin = function() {
+        return parseInt($sliderList.css("margin-left"), 10);
+      };
+      
+      var isAtBeginning = function() {
+        return getLeftMargin() === 0;
+      };
+
+      var isAtEnd = function() {
+        return getLeftMargin() === -endMargin;
+      };
+
+      var animateSlider = function(direction, duration, callback) {
+        $sliderList.stop(true, true).animate({
+          "margin-left" : direction + "=300px"
+        }, duration, callback);
+      }
+
+      var animateSliderToMargin = function(margin, duration, callback) {
+        $sliderList.stop(true, true).animate({
+          "margin-left": margin
+        }, duration, callback);
+      };
     });
   };
 }(jQuery);
